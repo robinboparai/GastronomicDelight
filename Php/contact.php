a449959e2e9a22e643a7b21f2aa25b8a4115e01d
robinboparai <155215328+robinboparai@users.noreply.github.com>
2024-02-01 09:59:00 +0530

    Add files via upload

---
+++ Php/contact.php
@@ -0,0 +1,21 @@
+<?php
+
+if ( isset($_POST['email']) && isset($_POST['name']) && isset($_POST['subject']) && isset($_POST['message']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
+ 
+
+  $test = "/(content-type|bcc:|cc:|to:)/i";
+  foreach ( $_POST as $key => $val ) {
+    if ( preg_match( $test, $val ) ) {
+      exit;
+    }
+  }
+
+$headers = 'From: ' . $_POST["name"] . '<' . $_POST["email"] . '>' . "\r\n" .
+    'Reply-To: ' . $_POST["email"] . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+  //
+  mail( "robinboparai", $_POST['subject'], $_POST['message'], $headers );
+
+}
+?>
